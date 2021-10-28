c93002bd3068ac136575f706b7776412c761f605
Andrew <diver.vlz@gmail.com>
2021-10-28 15:37:41 +0300

    Create new app "email_sender"

--- NewsBlog/urls.py
+++ NewsBlog/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from latest_news.views import index
+from latest_news.views import index, sender
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('send/', sender),
 ]
--- latest_news/views.py
+++ latest_news/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def index(request):
     return HttpResponse('<h1>Hello</h1>')
 
+def sender(request):
+    return HttpResponse('<h1>Fax Page</h1>')
