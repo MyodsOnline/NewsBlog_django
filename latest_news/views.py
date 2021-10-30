718dba6f8ab6d34fb947986dca9d4f2e6d52833c
Andrew <diver.vlz@gmail.com>
2021-10-30 17:49:06 +0300

    template in index.html changed +view.py

--- latest_news/views.py
+++ latest_news/views.py
@@ -6,10 +6,12 @@ from .models import News, Category
 
 def index(request):
     orders = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'title': 'Order_list',
         'content': 'Orders_list',
         'orders': orders,
+        'categories': categories,
     }
     return render(request, 'latest_news/index.html', context)
 
