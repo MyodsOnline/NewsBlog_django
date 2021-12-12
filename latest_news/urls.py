1b14d385a27c740164b9ce930f9e794536df0f08
Andrew <diver.vlz@gmail.com>
2021-12-13 00:36:24 +0300

    update view for latest_news

--- latest_news/urls.py
+++ latest_news/urls.py
@@ -5,8 +5,8 @@ from latest_news.views import *
 urlpatterns = [
     path('', index, name='index'),
     path('category/<int:category_id>/', get_category, name='category'),
-    path('order/<int:order_id>/', get_order, name='order'),
-    path('order/<int:order_id>/edit/', OrderUpdateView.as_view(), name='order_edit'),
+    # path('order/<int:order_id>/', get_order, name='order'),
+    path('order/<int:pk>/', OrderUpdateView.as_view(), name='order_edit'),
     path('order/add/', add_order, name='add_order'),
 ]
 
